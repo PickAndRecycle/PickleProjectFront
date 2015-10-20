a4cda7a7ac648c81b125c57183151c1226c8f9c6
adiyasa <adiyasanauval@gmail.com>
2015-10-20 22:18:31 +0700

    Basic Take Picture

--- app/src/main/java/com/pickle/pickleproject/Home.java
+++ app/src/main/java/com/pickle/pickleproject/Home.java
@@ -1,18 +1,51 @@
 package com.pickle.pickleproject;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.File;
 
-public class Home extends AppCompatActivity {
-
-    @Override
+public class Home extends Activity {
+    int CAM_REQUEST =1;
+    Button Camera;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Camera = (Button) findViewById(R.id.Camera_Button);
+        Camera.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+
+            }
+        });
 
         }
+    private File getFile(){
+        File folder = new File("sdcard/camera_app");
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+        File image_file = new File(folder,"cam_image.jpg");
+        return image_file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path = "sdcard/camera_app/cam_image.jpg";
+
+
+    }
 }
