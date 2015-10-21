d5c353a92ab257310c05beed71abc9e1d3160b3f
adiyasa <adiyasanauval@gmail.com>
2015-10-21 22:54:41 +0700

    basic swipe gesture

--- app/src/main/java/com/pickle/pickleproject/Home.java
+++ app/src/main/java/com/pickle/pickleproject/Home.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Button;
@@ -14,6 +17,8 @@ import android.widget.Button;
 public class Home extends Activity {
     int CAM_REQUEST =1;
     ImageButton Camera;
+    private GestureDetector gestureDetector;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
@@ -29,6 +34,7 @@ public class Home extends Activity {
 
             }
         });
+
         Button Swipebutton;
         Swipebutton = (Button) findViewById(R.id.swiperightbtn);
 
@@ -38,6 +44,8 @@ public class Home extends Activity {
                 changePick();
             }
         });
+
+        gestureDetector = new GestureDetector(new SwipeGestureDetector());
 
         }
 
@@ -59,7 +67,46 @@ public class Home extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         String path = "sdcard/Pickle/cam_image.jpg";
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+    private class SwipeGestureDetector
+            extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float velocityX, float velocityY) {
+            try {
+                float diffAbs = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
 
+                if (diffAbs > SWIPE_MAX_OFF_PATH)
+                    return false;
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Home.this.changePick();
+
+                    // Right swipe
+                } //else if (-diff > SWIPE_MIN_DISTANCE
+                    //    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                  //  YourActivity.this.onRightSwipe();
+                //}
+            } catch (Exception e) {
+                Log.e("YourActivity", "Error on gestures");
+            }
+            return false;
+        }
     }
 }
+
