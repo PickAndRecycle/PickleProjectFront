08faf760f00ade7f9a485bf5c21185e7c05f2294
Hakeem <hakeemrdaud@gmail.com>
2015-10-22 22:51:01 +0700

    try animation

--- app/src/main/java/com/pickle/pickleproject/picklejar.java
+++ app/src/main/java/com/pickle/pickleproject/picklejar.java
@@ -3,6 +3,9 @@ package com.pickle.pickleproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class picklejar extends AppCompatActivity {
 
@@ -16,5 +19,54 @@ public class picklejar extends AppCompatActivity {
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
         this.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    private void changeNews() {
+        Intent intent = new Intent(this, newsfeed.class);
+        startActivity(intent);
+        this.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    private void onLeftSwipe() {
+        changeHome();
+    }
+    private void onRightSwipe() {
+        changeNews();
+    }
+
+    // Private class for gestures
+    private class SwipeGestureDetector
+            extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float velocityX, float velocityY) {
+            try {
+                float diffAbs = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
+
+                if (diffAbs > SWIPE_MAX_OFF_PATH)
+                    return false;
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    picklejar.this.onLeftSwipe();
+
+                    // Right swipe
+                } else if (-diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    picklejar.this.onRightSwipe();
+                }
+            } catch (Exception e) {
+                Log.e("YourActivity", "Error on gestures");
+            }
+            return false;
+        }
     }
 }
