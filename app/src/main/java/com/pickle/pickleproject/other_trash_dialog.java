e9755b0b34bd13a37ac23d61b46b74251e75fd35
Tsabita Vera Cyavrilla <tsabita_fnloverz@yahoo.com>
2015-10-22 23:26:00 +0700

    Added dialog for unused goods and other waste types.

---
+++ app/src/main/java/com/pickle/pickleproject/other_trash_dialog.java
@@ -0,0 +1,18 @@
+package com.pickle.pickleproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class other_trash_dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_trash_dialog);
+    }
+
+}
---
+++ app/src/main/java/com/pickle/pickleproject/unused_goods_dialog.java
@@ -0,0 +1,21 @@
+package com.pickle.pickleproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
+public class unused_goods_dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unused_goods_dialog);
+
+    }
+
+}
