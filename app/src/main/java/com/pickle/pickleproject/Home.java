d2e4c475258f61ccdbebc4c3a2ab573c33c2341e
DANIEL <danieldeeeem@gmail.com>
2015-10-23 03:35:00 +0700

    membersihkan kotoran

--- app/src/main/java/com/pickle/pickleproject/Home.java
+++ app/src/main/java/com/pickle/pickleproject/Home.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import java.io.File;
 
