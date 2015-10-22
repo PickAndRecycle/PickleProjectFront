4b5df830d34c53bbdf75c9b0240b941728ab0915
Tsabita Vera Cyavrilla <tsabita_fnloverz@yahoo.com>
2015-10-22 14:18:34 +0700

    Added mipmap for categories and layout for throw category

---
+++ app/src/main/java/com/pickle/pickleproject/Thrower_Info.java
@@ -0,0 +1,15 @@
+package com.pickle.pickleproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Thrower_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thrower_info);
+
+    }
+
+}
