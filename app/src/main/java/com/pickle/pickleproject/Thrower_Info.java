f59528443659f00a2fc2285696031f907900cd54
Hakeem <hakeemrdaud@gmail.com>
2015-10-27 09:27:15 +0700

    add call intent

--- app/src/main/java/com/pickle/pickleproject/Thrower_Info.java
+++ app/src/main/java/com/pickle/pickleproject/Thrower_Info.java
@@ -1,7 +1,14 @@
 package com.pickle.pickleproject;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Thrower_Info extends AppCompatActivity {
 
@@ -10,6 +17,42 @@ public class Thrower_Info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thrower_info);
 
+        Button back_button = (Button) findViewById(R.id.back_button);
+        Button call_button = (Button) findViewById(R.id.call_button);
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeBack();
+            }
+        });
+        call_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeCall();
+            }
+        });
+    }
+
+    private void changeBack() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    private void changeCall() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:08127648183"));
+        if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        startActivity(intent);
     }
 
 }
