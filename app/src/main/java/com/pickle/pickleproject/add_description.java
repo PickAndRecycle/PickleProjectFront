8fe3d1f97a5007277b8015d7d5bccf5ad64d87fe
dennisdarwis <dens07.dad@gmail.com>
2015-11-02 17:15:43 +0700

    tes

--- app/src/main/java/com/pickle/pickleproject/add_description.java
+++ app/src/main/java/com/pickle/pickleproject/add_description.java
@@ -4,11 +4,20 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+
+import com.pickle.pickleprojectmodel.Trash;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class add_description extends AppCompatActivity {
 
@@ -23,11 +32,35 @@ public class add_description extends AppCompatActivity {
 
         Button NextButton = (Button) findViewById(R.id.nextButton);
         NextButton.setOnClickListener(new View.OnClickListener() {
+
+            private EditText descForm;
+            private final static String STORETEXT="storetext.txt";
+
             @Override
             public void onClick(View v) {
                 changeNext();
+                descForm = (EditText) findViewById(R.id.editText);
+
+                try {
+                    OutputStreamWriter out = new OutputStreamWriter(openFileOutput(STORETEXT, 0));
+                    out.write(descForm.getText().toString()+"\n");
+                    out.close();
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.makeText(add_description.this, "The contents are saved in the file.", toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                //TOAST FOR DEBUGGING
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+                toast.makeText(add_description.this, descForm.getText(), toast.LENGTH_SHORT).show();
             }
         });
+
 
         Button HomeButton = (Button) findViewById(R.id.backButton);
         HomeButton.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +70,7 @@ public class add_description extends AppCompatActivity {
             }
         });
     }
+
     private void changeNext(){
         Intent intent = new Intent(this, Throw_or_Report.class);
         startActivity(intent);
