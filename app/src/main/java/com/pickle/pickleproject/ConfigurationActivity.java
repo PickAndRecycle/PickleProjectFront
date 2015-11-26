1a82901cd22a6e94b2a2b86fda78941351d63628
Tsabita Vera Cyavrilla <tsabita_fnloverz@yahoo.com>
2015-11-26 23:18:57 +0700

    Localization and string refactoring.

--- app/src/main/java/com/pickle/pickleproject/ConfigurationActivity.java
+++ app/src/main/java/com/pickle/pickleproject/ConfigurationActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -44,18 +45,19 @@ public class ConfigurationActivity extends AppCompatActivity {
             }
         });
         final Configuration config = new Configuration();
+        final Locale locale = Locale.getDefault();
         Button language = (Button) findViewById(R.id.ChangeLanguageButton);
         language.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (config.locale.equals(Locale.ENGLISH)){
+                if (locale.toString().toLowerCase().equals("en_us")){
                     Locale indonesia = new Locale("in");
                     Locale.setDefault(indonesia);
                     config.locale = indonesia;
                     getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
                 }
                 else {
-                    Locale english = new Locale("en");
+                    Locale english = new Locale("en_us");
                     Locale.setDefault(english);
                     config.locale = english;
                     getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
