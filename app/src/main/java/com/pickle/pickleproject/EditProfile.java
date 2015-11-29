cca745dab06268e339d817d7e56d253f403884bd
Tsabita Vera Cyavrilla <tsabita_fnloverz@yahoo.com>
2015-11-29 20:36:40 +0700

    Another localization and string refactoring, including added back button in edit profile.

--- app/src/main/java/com/pickle/pickleproject/EditProfile.java
+++ app/src/main/java/com/pickle/pickleproject/EditProfile.java
@@ -3,13 +3,11 @@ package com.pickle.pickleproject;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -45,6 +43,14 @@ public class EditProfile extends AppCompatActivity {
         phone.setText(account.getPhone_number());
 
         MontserratButton saveButton = (MontserratButton) findViewById(R.id.saveButton);
+        ImageButton backIcon = (ImageButton) findViewById(R.id.backIcon);
+
+        backIcon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
 
         mQueue = CustomVolleyRequestQueue.getInstance(this.getApplicationContext()).getRequestQueue();
 
