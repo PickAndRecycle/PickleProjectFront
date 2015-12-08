8ca9ae61c4ddbbc1c736b2ce336fa33643f6fc8f
dennisdarwis <dens07.dad@gmail.com>
2015-12-08 12:38:50 +0700

    adding for POST gmail information to database if the gmail has not been stored yet. adding for GET gmail information from database if the gmail has already been stored.

--- app/src/main/java/com/pickle/pickleproject/SignInActivity.java
+++ app/src/main/java/com/pickle/pickleproject/SignInActivity.java
@@ -1,13 +1,22 @@
 package com.pickle.pickleproject;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -18,7 +27,22 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.pickle.pickleproject.R;
+import com.pickle.pickleprojectmodel.Account;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Activity to demonstrate basic retrieval of the Google user's ID, email address, and basic
@@ -36,6 +60,10 @@ public class SignInActivity extends AppCompatActivity implements
     private ProgressDialog mProgressDialog;
 
     private boolean alreadySignIn;
+
+    public static final String PREFS_NAME = "PicklePrefs";
+
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,21 +155,138 @@ public class SignInActivity extends AppCompatActivity implements
         Log.d(TAG, "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
+            final GoogleSignInAccount acct = result.getSignInAccount();
 
             acct.getEmail();
             acct.getPhotoUrl();
             acct.getId();
             acct.getIdToken();
-            Log.d("Account lists", "email: "+acct.getEmail()+" "+ "photo url: "+acct.getPhotoUrl()+" "+"ID: "+acct.getId()+" "+"ID Token: "+acct.getIdToken());
+            final String email = acct.getEmail();
+            String nameOnly = email.substring(0,email.indexOf('@'));
+            Log.d("Account lists", "email: " + acct.getEmail() + " " + "photo url: " + acct.getPhotoUrl() + " " + "ID: " + acct.getId() + " " + "ID Token: " + acct.getIdToken());
 
+            //GCP
+            final String url = "http://104.155.237.238:8080/account/";
+            //CHECK if email is in database
+            final CustomJSONObjectRequest jsonRequest = new CustomJSONObjectRequest(Request.Method.GET, url, new JSONObject(), new Response.Listener<JSONObject>() {
+
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONObject parentObject = response;
+                        Log.d("json:", response.getString("result"));
+                        JSONArray parentArray = parentObject.getJSONArray("result");
+                        List<Account> accountList = new ArrayList<Account>();
+                        alreadySignIn = false;
+                        final String username = email;
+                        Log.d("username", username);
+                        for (int i = 0; i < parentArray.length(); i++) {
+                            JSONObject finalObject = parentArray.getJSONObject(i);
+                            Account accountObj;
+                            GsonBuilder gsonBuilder = new GsonBuilder();
+
+                            final Gson gson = gsonBuilder.create();
+                            accountObj = gson.fromJson(String.valueOf(finalObject), Account.class);
+                            Log.d("username", accountObj.getUsername());
+                            Log.d("password", accountObj.getPassword());
+
+                            if ((accountObj.getUsername().equals(username))) {
+                                String getSecure_id = accountObj.getId();
+                                String getUsername = accountObj.getUsername();
+                                Integer getPoint = accountObj.getPoint();
+                                String getEmail = accountObj.getEmail();
+
+                                String x = "secure id= " + getSecure_id + ", " + "username= " + getUsername + ", " + "point= " + getPoint + ", " + "email= " + getEmail;
+
+                                //put secure_id into SharedPreferences
+                                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                                SharedPreferences.Editor editor = settings.edit();
+                                editor.putString("secure_id", getSecure_id);
+                                editor.putString("username", getUsername);
+                                editor.putInt("point", getPoint);
+                                editor.putString("email", getEmail);
+                                editor.putString("valid", "1");
+                                editor.commit();
+                                //TOAST
+                                    /*
+                                    Toast boom = new Toast(getApplicationContext());
+                                    boom.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+                                    boom.makeText(SignIn.this, x, boom.LENGTH_SHORT).show();
+                                    */
+
+                                alreadySignIn = true;
+                                break;
+                            }
+                        }
+                        if (alreadySignIn) {
+                            signIn();
+                        } else {
+                            Account account = new Account();
+                            account.setUsername(email);
+                            account.setEmail(acct.getEmail());
+                            account.setPassword(acct.getIdToken());
+                            mQueue = CustomVolleyRequestQueue.getInstance(SignInActivity.this).getRequestQueue();
+                            GsonBuilder gsonBuilder = new GsonBuilder();
+
+                            gsonBuilder.registerTypeAdapter(Account.class, new AccountSerializer());
+                            Gson gson = gsonBuilder.create();
+                            String json = gson.toJson(account);
+                            Log.d("json", json);
+                            try {
+                                final CustomJSONObjectRequest jsonRequest = new CustomJSONObjectRequest(Request.Method.POST, url, new JSONObject(json), new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            Log.d("result", response.getString("result"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Log.d("Error:", error.getMessage());
+                                    }
+                                });
+                                jsonRequest.setRetryPolicy(new DefaultRetryPolicy(60000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                                mQueue.add(jsonRequest);
+                                //TOAST
+                                Toast boom = new Toast(getApplicationContext());
+                                boom.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+                                boom.makeText(SignInActivity.this, "Your e-mail finally registered.", boom.LENGTH_SHORT).show();
+
+
+                            }
+                            catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+                    } catch (JSONException e) {
+                        //TOAST
+                        Toast boom = new Toast(getApplicationContext());
+                        boom.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+                        boom.makeText(SignInActivity.this, "Invalid username or password.", boom.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Error", error.toString());
+                }
+            });
+            jsonRequest.setRetryPolicy(new DefaultRetryPolicy(60000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+/*
             if(alreadySignIn){
                 // Session Storage ganti jadi account kita
             }
             else{
                 // Masukin database
             }
-
+*/
 
 
 
@@ -158,6 +303,9 @@ public class SignInActivity extends AppCompatActivity implements
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        //PUT EMAIL AND SECURE_ID
+
     }
     // [END signIn]
 
@@ -236,6 +384,21 @@ public class SignInActivity extends AppCompatActivity implements
             case R.id.disconnect_button:
                 revokeAccess();
                 break;
+        }
+    }
+
+    private class AccountSerializer implements JsonSerializer<Account> {
+        @Override
+        public JsonElement serialize(Account src, Type typeOfSrc, JsonSerializationContext context) {
+            JsonObject object = new JsonObject();
+            object.addProperty("id",src.getId());
+            object.addProperty("username",src.getUsername());
+            object.addProperty("email",src.getEmail());
+            object.addProperty("password",src.getPassword());
+            object.addProperty("phone_number",src.getPhone_number());
+            object.addProperty("photo_url",src.getProfile_picture_url());
+
+            return object;
         }
     }
 }
